8b4db99fea0dafa1c4c4ed87fe8477ea6788545d
ChamodhiSatharasinghe <56638407+ChamodhiSatharasinghe@users.noreply.github.com>
2021-07-09 02:22:34 +0530

    Signup Authentication and handbook components

--- webfrontend/src/App.js
+++ webfrontend/src/App.js
@@ -5,7 +5,9 @@ import Navigation from "./components/common/header/Navigation";
 import Home from "./components/static/Home";
 import About from "./components/static/About";
 import Contact from "./components/static/Contact";
-
+import Store from "./components/store/Store";
+import Handbook from "./components/handbook/Handbook";
+import Signup from "./components/authentication/Signup";
 
 function App() {
   return (
@@ -14,7 +16,9 @@ function App() {
       <Home />
       <About />
       <Contact />
-      
+      <Store />
+      <Handbook />
+      <Signup />
     </div>
   );
 }
---
+++ webfrontend/src/components/authentication/Signup.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Signup() {
+    return(
+        <div>
+            <h1>This is Signup page</h1>
+            <p>Shashreeka recognized that the growing demand for organic crop farming required an increased focus on providing an all-natural solution to boost soil nutrition. Rather than make unproven claims about our fertilizer, we focus on education, testing, and proper use to document results. This process may be slower, but it ensures that we support the growth of sustainable agriculture around the world.</p>
+        </div>
+    )
+}
+
+export default Signup;
