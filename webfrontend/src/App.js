22eec0678d17048d38fd4ce9e00b184c31688be8
ChamodhiSatharasinghe <56638407+ChamodhiSatharasinghe@users.noreply.github.com>
2021-07-09 02:11:21 +0530

    Static page home, about and contact

--- webfrontend/src/App.js
+++ webfrontend/src/App.js
@@ -2,12 +2,18 @@ import React from "react";
 // import Colors from "./assets/colors/Colors";
 // import './App.css';
 import Navigation from "./components/common/header/Navigation";
+import Home from "./components/static/Home";
+import About from "./components/static/About";
+import Contact from "./components/static/Contact";
 
 
 function App() {
   return (
     <div className="App">
       <Navigation/>
+      <Home />
+      <About />
+      <Contact />
       
     </div>
   );
