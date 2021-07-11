ed7bf9517564a1a0513e53ea9a3f9e686104dc07
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-07-11 15:53:09 +0530

    Admin Panel-home -widget added

--- webfrontend/src/components/company/Admin/pages/home/Home.js
+++ webfrontend/src/components/company/Admin/pages/home/Home.js
@@ -3,6 +3,9 @@ import './home.css';
 import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo';
 import Chart from '../../components/chart/Chart';
 import { UserData } from '../../dummyData';
+import WidgetLg from '../../components/widget/WidgetLg';
+import WidgetSm from '../../components/widget/WidgetSm';
+
 
 export default function Home() {
     return (
@@ -10,6 +13,12 @@ export default function Home() {
 
         <FeaturedInfo/>
         <Chart data={UserData} title="User Analytics" grid dataKey="Active User"/>
+
+        <div className="homeWidgets">
+                <WidgetSm/>
+               <WidgetLg/>
+              
+            </div>
             
         </div>
     )
