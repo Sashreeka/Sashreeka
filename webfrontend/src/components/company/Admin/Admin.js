5a0bfd72b6f2a53733b140e8303858e9ee67b008
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-07-11 14:36:07 +0530

    Admin Panel-home -feature added

--- webfrontend/src/components/company/Admin/Admin.js
+++ webfrontend/src/components/company/Admin/Admin.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import './admin.css';
-import Sidebar from './sidebar/Sidebar';
+import Home from './pages/home/Home';
+import Sidebar from './components/sidebar/Sidebar';
 
 
 export default function Admin() {
     return (
         <div>
-            <div className="container">
+            <div className="admincon">
             <Sidebar/>
-                <div className="others">
-                    Other Page
-                </div>
+            <Home/>
 
             </div>
        
