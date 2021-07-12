c4c5e247275955fb1729c0bc84ca96b66b4ca95f
Anuki-UCSC <2018cs030@stu.ucsc.cmb.ac.lk>
2021-07-12 23:07:47 +0530

    bug fixed

---
+++ FarmerApp/assets/data/itemsData.js
@@ -0,0 +1,40 @@
+const itemsData=[
+    {
+        id:1,
+        image: require("../images/bio1.png"),
+        title: "Bio Vaccine",
+        description: "Bio VaccineBio ",
+        rating: 5.0,  
+    },
+    {
+        id:2,
+        image: require("../images/bio2.png"),
+        title: "Bio Gold",
+        description: "Bio GoldBio",
+        rating: 4.3,  
+    },
+    {
+        id:3,
+        image: require("../images/bio3.png"),
+        title: "Bio Phos",
+        description: "Bio PhosBio",
+        rating: 4.3,  
+    },
+    {
+        id:4,
+        image: require("../images/bio1.png"),
+        title: "Bio Vaccine",
+        description: "Bio PhosBio",
+        rating: 4.3,  
+    },
+    {
+        id:5,
+        image: require("../images/bio1.png"),
+        title: "Bio Vaccine",
+        description: "Bio PhosBio",
+        rating: 4.3,  
+    },
+];
+
+
+export default itemsData;
--- FarmerApp/components/Home.js
+++ FarmerApp/components/Home.js
@@ -49,7 +49,9 @@ export default Home = ()=>{
                 <Text style={styles.titlesTitle}>Organic Fertilizer</Text>
             </View>
 
-            <ScrollView contentInsetAdjustmentBehavior="automatic" showsVerticalScrollIndicator={false}>
+            {/* <ScrollView contentInsetAdjustmentBehavior="automatic" showsVerticalScrollIndicator={false}
+            key={Math.random}
+            > */}
             {/* Search */}
             <View style={styles.searchWrapper}>
                 <Feather name="search" size={16} color={colors.textDark}/>
@@ -76,7 +78,9 @@ export default Home = ()=>{
             <View style={styles.itemsWrapper}>
                 <Text style={styles.itemsTitle}>Shop Items</Text>
                 {itemsData.map((item)=>(
-                     <View style={[styles.itemsCardwrapper,
+                     <View 
+                     key={item.id}
+                     style={[styles.itemsCardwrapper,
                      {
                          marginTop:item.id==1 ? 10 : 20,
                      }
@@ -104,8 +108,209 @@ export default Home = ()=>{
                 )   
                 )}
             </View>
-            </ScrollView>
+            {/* </ScrollView> */}
         </View>
         
     );
 }
+
+
+
+const styles=StyleSheet.create({
+    container:{
+        flex:1,
+    },
+    headerWrapper:{
+        flexDirection:"row",
+        justifyContent:"space-between",
+        paddingHorizontal:20,
+        paddingTop:15,
+        alignItems:"center",
+        backgroundColor:colors.secondary,
+    },
+    profileImage:{
+        width:40,
+        height:40,
+        borderRadius:40,
+        
+    },
+    profileImage:{
+        width:40,
+        height:40,
+        borderRadius:40,
+        
+    },
+    titlesTitle:{
+        // fontFamily:"Roboto-bold",
+        fontSize:32,
+        color:colors.textDark,
+        paddingLeft:10,
+    },
+    titleView:{
+        backgroundColor:colors.secondary,
+        padding:10,
+        paddingBottom:10,
+        borderBottomLeftRadius:40,
+        borderBottomRightRadius:40,
+    },
+    searchWrapper:{
+        paddingTop:20,
+        paddingBottom:10,
+        flexDirection:"row",
+        alignItems:"center",  
+        borderBottomColor:colors.textlight,
+        borderBottomWidth:2,
+        marginHorizontal:20,
+    },
+    search:{
+        marginLeft:20,
+    },
+    searchText:{
+        fontSize:15,
+        color:colors.textlight,
+    },
+    categoriesWrapper:{
+       marginTop:30,
+
+    },
+    categoriesTitle:{
+        fontWeight:"bold",
+        fontSize:20,
+        paddingHorizontal:20,
+    },
+
+    categoriesListWrapper:{
+        paddingTop:15,
+        paddingBottom:20,
+        // paddingLeft:20,
+    },
+
+    categoryItemWrapper:{
+       backgroundColor:colors.secondaryT50,
+       display:'flex',
+       marginRight:20,
+       borderRadius:20,
+       width:130,
+       padding:10,
+       alignItems:"center",
+       justifyContent:"center",
+       shadowColor:"black",
+       shadowOffset:{
+           width:1,
+           height:2,
+       },
+       shadowOpacity:0.05,
+       shadowRadius:10,
+       elevation:2,
+
+    },
+
+    categoryItemImage:{
+        marginTop:25,
+        alignSelf:"center",
+        marginHorizontal:27,
+    },
+
+    categoryItemTitle:{
+        display:"flex",
+        textAlign:"center",
+        fontSize:16,
+        fontWeight:"600",
+        marginTop:10,
+        textTransform:'uppercase',
+    },
+
+    categorySelectWrapper:{
+        height:30,
+        alignSelf:"center",
+        justifyContent:"center",
+        
+    },
+
+    categoryItemimage:{
+
+    },
+
+
+    itemsWrapper:{
+       paddingHorizontal:20,
+    },
+
+    itemsTitle:{
+        fontSize:20,
+        fontWeight:"bold",  
+    },
+
+    itemsCardwrapper:{
+        backgroundColor:colors.background,
+        borderRadius:15,
+        paddingTop:20,
+        paddingLeft:20,
+        paddingBottom:20,
+        flexDirection:"row",
+        shadowColor:"black",
+        shadowOffset:{
+            width:1,
+            height:2,
+        },
+        shadowOpacity:0.05,
+        shadowRadius:10,
+        elevation:2,
+
+    },
+
+    itemWrapperMain:{
+        flexDirection:"row",
+        alignItems:"center",
+
+    },
+
+    itemTitleMain:{
+        fontSize:20 ,
+        color: colors.textlight,
+        fontWeight:'700',
+    },
+    itemDescription:{
+
+    },
+
+    itemDescriptionWrapper:{
+       marginTop:10,
+
+    },
+
+    itemDescription:{
+        fontSize:14,
+        color:colors.textDark,
+    },
+
+    itemUnitWeight:{
+        fontSize:12,
+        color:colors.textlight,
+        marginTop:5,
+    },
+    itemADDbutton:{
+        backgroundColor:colors.primary,
+        width:150,
+        alignItems:"center",
+        justifyContent:"center",
+        padding:10,
+        borderRadius:15,
+    },
+
+    itemCardWrapOuter:{
+        flexDirection:"row",
+    
+    },
+
+    itemcardRight:{
+        marginTop:20,
+    },
+    itemcardimage:{
+        width:210,
+        height:125,
+        resizeMode:"contain",
+
+    },
+
+})
