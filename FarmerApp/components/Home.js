65b880132cffa1003bd6b4681ca73a5fe8f74ff1
Anuki-UCSC <2018cs030@stu.ucsc.cmb.ac.lk>
2021-07-12 20:39:48 +0530

    home -imports

---
+++ FarmerApp/components/Home.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import {FlatList,StatusBar,View, Text, StyleSheet,SafeAreaView,Image, ScrollView} from "react-native";
+import Feather from "react-native-vector-icons/Feather";
+import fontAwsome from "react-native-vector-icons/FontAwesome";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+
+import categoriesData from '../assets/data/categoriesData';
+import itemsData from '../assets/data/itemsData';
+import colors from '../assets/colors/colors';
+
+Feather.loadFont();
+
