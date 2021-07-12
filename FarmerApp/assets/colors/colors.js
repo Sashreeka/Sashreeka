5f7c5b8a56a4c6cd9030b02ed235e0346782d699
Anuki-UCSC <2018cs030@stu.ucsc.cmb.ac.lk>
2021-07-12 20:47:15 +0530

    farmer mobile app -assets

---
+++ FarmerApp/assets/colors/colors.js
@@ -0,0 +1,12 @@
+const colors={
+    background:"#FFFFFF",
+    backgroundAsh:"#F0F0F0",
+    // primary:"#A5E175",
+    primary:"#89b963",
+    secondary:"#B7F785",
+    textlight:"#969696",
+    textDark:"#000000",
+    secondaryT50:"#b6f7859c"
+};
+
+export default colors;
