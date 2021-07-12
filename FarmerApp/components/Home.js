b2afea8dbb113b15d7c94a3aa1cb8d99423d2959
Anuki-UCSC <2018cs030@stu.ucsc.cmb.ac.lk>
2021-07-12 20:41:56 +0530

    Home -header

--- FarmerApp/components/Home.js
+++ FarmerApp/components/Home.js
@@ -10,3 +10,38 @@ import colors from '../assets/colors/colors';
 
 Feather.loadFont();
 
+export default Home = ()=>{
+
+    
+
+    return(
+        <View style={styles.container}>
+            
+            <StatusBar backgroundColor={colors.primary}/>
+            <SafeAreaView>
+                <View style={styles.headerWrapper}>
+                    <Image source={require("../assets/images/profileimg_girl.jpg")} style={styles.profileImage}/>
+                    <Feather name="menu" size={24} color={colors.textDark}></Feather>
+                </View>
+                
+            </SafeAreaView>
+            {/* titles */}
+            <View style={styles.titleView}>
+                <Text style={styles.titlesTitle}>Organic Fertilizer</Text>
+            </View>
+
+            <ScrollView contentInsetAdjustmentBehavior="automatic" showsVerticalScrollIndicator={false}>
+            {/* Search */}
+            <View style={styles.searchWrapper}>
+                <Feather name="search" size={16} color={colors.textDark}/>
+                <View style={styles.search}>
+                    <Text style={styles.searchText}>Search...</Text>
+                </View>
+            </View>
+
+            
+            </ScrollView>
+        </View>
+        
+    );
+}
