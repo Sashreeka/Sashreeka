95a1892fe58e4abe76d97f3b2e860dcbc938f38e
Anuki-UCSC <2018cs030@stu.ucsc.cmb.ac.lk>
2021-07-12 20:43:18 +0530

    Home -items list UI

--- FarmerApp/components/Home.js
+++ FarmerApp/components/Home.js
@@ -54,7 +54,37 @@ export default Home = ()=>{
             </View>
 
             {/* items list */}
-            
+            <View style={styles.itemsWrapper}>
+                <Text style={styles.itemsTitle}>Shop Items</Text>
+                {itemsData.map((item)=>(
+                     <View style={[styles.itemsCardwrapper,
+                     {
+                         marginTop:item.id==1 ? 10 : 20,
+                     }
+                     ]}>
+                         <View style={styles.itemCardWrapOuter}>
+                            <View>
+                                <View style={styles.itemWrapperMain}>
+                                    <Text style={styles.itemTitleMain}>{item.title}</Text>  
+                                </View>
+                                <View style={styles.itemDescriptionWrapper}>
+                                    <Text style={styles.itemDescription}>{item.description}</Text>
+                                    <Text style={styles.itemUnitWeight}>{item.UnitWeight}</Text>
+                                </View>
+                                <View style={styles.itemADDbutton}>
+                                    <Text>ADD<Feather name="plus" size={15} color='#000'/>
+                                    </Text>
+                                </View>
+                            </View>
+                            <View styles={styles.itemcardRight}>
+                                <Image source={item.image} styles={styles.itemcardimage}/>
+                            </View>
+                         </View>
+                         
+                 </View>
+                )   
+                )}
+            </View>
             </ScrollView>
         </View>
         
