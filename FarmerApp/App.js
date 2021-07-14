a144aae43ed5777a7de42ffd99f7c59363ca4294
Anuki-UCSC <2018cs030@stu.ucsc.cmb.ac.lk>
2021-07-15 01:00:24 +0530

    item page basic ui

--- FarmerApp/App.js
+++ FarmerApp/App.js
@@ -4,15 +4,19 @@ import {Text} from "react-native";
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import Home from "./components/Home";
+// import Home from "./components/Home";
+// import Main from "./components/Main";
+import Main from "./components/ItemPage";
+import ItemPage from './components/ItemPage';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
+    
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} 
+        <Stack.Screen name="ItemPage" component={ItemPage} 
         options={{headerShown: false,}}/>
       </Stack.Navigator>
     </NavigationContainer>
