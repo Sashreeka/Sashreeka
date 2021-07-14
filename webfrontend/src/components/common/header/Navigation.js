7792356f2e7fbe39aa538ee286df05d607bd90da
shamalipathirana <shyamalee.123.pathirana@gmail.com>
2021-07-14 14:03:26 +0530

    Change navigation bar

--- webfrontend/src/components/common/header/Navigation.js
+++ webfrontend/src/components/common/header/Navigation.js
@@ -1,25 +1,33 @@
-import React from "react";
+import React, {Component} from "react";
 //import {Link} from 'react-router-dom';
 import "./Navigation.css";
 import {MenuItems} from "./MenuItems"
+import { render } from "@testing-library/react";
+//import { render } from "@testing-library/react";
+class Navigation extends Component{
 
-function Navigation() {
+  state={ clicked :false}
+  handleClick = () =>{
+    this.setState({clicked :!this.state.clicked})
+  }
+  render(){
   return (
-
+    
     <nav className="NavbarItems">
       <h1 className="navbar-logo">Shashreeka</h1>
-      <div className="menu-icon">
+      <div className="menu-icon" onClick={this.handleClick}>
+        <i className={this.state.Clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
 
       </div>
-      <ul>
+      
+      <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
         {MenuItems.map((item,index)=>{
-
-        return (
-          <li key={index}>
-            <a className={item.cName} href={item.url}>
+          return (
+            <li key={index}>
+              <a className={item.cName} href={item.url}>
               {item.title}
-            </a>
-          </li>
+              </a>
+            </li>
         )
         
         
@@ -66,7 +74,11 @@ function Navigation() {
         </div>
     </nav>*/
     
-  );
+  )
 }
+}
+
+
+
 
 export default Navigation;
