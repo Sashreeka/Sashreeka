d8c0dc5d21cc6ebe22855e7e7e22067f8a353db8
shamalipathirana <shyamalee.123.pathirana@gmail.com>
2021-07-16 23:07:20 +0530

    Change Home.js

--- webfrontend/src/components/static/Home.js
+++ webfrontend/src/components/static/Home.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import './Home.css';
-
+// import img1 from '../../assests/images/Home/back-img-1.png'
+import img1 from '../../assets/images/Home/backImg1.png';
+ 
 function Home() { 
     return(
         <div>
-        <div className="Container">
+        <div className="container">
         <h1>Welcome to Shashreeka</h1>
         <h3>The Best Online Organic Fertilizer Market</h3>
         <p>Shashreeka recognized that the growing demand for organic <br/>crop farming required an <br/> increased focus on providing an all-natural solution to boost soil nutrition.
          Rather than make <br/>unproven claims about our fertilizer, we focus on education,<br/>testing, and proper use to document results. This process may be<br/> slower, but it ensures that we support the growth of <br/>
         sustainable agriculture around the world.
          </p>
-         <div className="hero-section-img">
-         <img src={require('../../assets/images/Home/back-img-1.png').default} />
+         <div className="home-hero-img-wrapper">
+         
+         <img src={img1} className="home-hero-img"/>
          </div>
 
         </div>
