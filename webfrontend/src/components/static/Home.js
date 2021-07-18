684a94e589d9239efd5d5a00f6e9e3ad843f0929
shamalipathirana <shyamalee.123.pathirana@gmail.com>
2021-07-18 19:58:22 +0530

    Create home page 1st section

--- webfrontend/src/components/static/Home.js
+++ webfrontend/src/components/static/Home.js
@@ -1,32 +1,34 @@
 import React from 'react';
 import './Home.css';
 // import img1 from '../../assests/images/Home/back-img-1.png'
-import img1 from '../../assets/images/Home/back-img-1.jpg';
- 
+import img1 from '../../assets/images/Home/img1.jpg';
+import {Link} from 'react-router-dom'
+
 function Home() { 
     return(
         
-               <div className="hero" >
-               <div class="row">
-               <div class="col1-1"></div>
-               <h1 className="hed1">Welcome to Shashreeka</h1>
+               <div className="home" >
+              {/*<img src={img1} height={309} width={626} position= 'absolute' top={10} left={10} />*/}
+               <div class="headerContainer">
+               
+               <h1>Welcome to Shashreeka</h1>
 
-                         <h3 className="hed2">The Best Online Organic Fertilizer Market</h3>
-                         <p>Shashreeka recognized that the growing demand for organic <br/>crop farming required an <br/> increased focus on providing an all-natural solution to boost soil nutrition.
+                         <h3>The Best Online Organic Fertilizer Market</h3>
+                         <p>Shashreeka recognized that the growing demand for organic <br/>crop farming required an increased focus on providing <br/>an all-natural solution to boost soil nutrition.
                            Rather than make <br/>unproven claims about our fertilizer, we focus on education,<br/>testing, and proper use to document results. This process may be<br/> slower, but it ensures that we support the growth of <br/>
                            sustainable agriculture around the world.
                          </p>
-                         
-                        <a  class="link1" href="">Get Started</a>
-                        
-                   </div>
-                   <div className="col-2">     
+                         <Link to ="/signup">
+                        <button>Get Started</button>
+                        </Link>
+                      
     
 
-                        <img src={img1} className="home-hero-img"/>
+                        </div>
+                        <img src={img1} height={309} width={626} position= 'absolute' top={10} left={10} />
                         </div>
                         
-                    </div>
+                    
                
             
         
