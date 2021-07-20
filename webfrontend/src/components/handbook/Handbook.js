84856d1718ffac75de8fade824304837fb92553e
shamalipathirana <shyamalee.123.pathirana@gmail.com>
2021-07-20 23:39:31 +0530

    Create Fertilizer handbook page

--- webfrontend/src/components/handbook/Handbook.js
+++ webfrontend/src/components/handbook/Handbook.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import '.Handbook.css';
 
 function Handbook() {
     return(
         <div>
-            <h1>This is Fertilizer Handbook</h1>
-            <p>Shashreeka recognized that the growing demand for organic crop farming required an increased focus on providing an all-natural solution to boost soil nutrition. Rather than make unproven claims about our fertilizer, we focus on education, testing, and proper use to document results. This process may be slower, but it ensures that we support the growth of sustainable agriculture around the world.</p>
+            <h1>Sashreeka Fertilizer Hand Book</h1>
+    <iframe src="https://www.doa.gov.lk/publications/books/ipns.pdf" width="100%" height="600px">
+    </iframe>
         </div>
     )
 }
