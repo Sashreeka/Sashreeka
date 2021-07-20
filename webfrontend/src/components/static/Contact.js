94201898c720ca65c041dd883067352565f302ce
shamalipathirana <shyamalee.123.pathirana@gmail.com>
2021-07-20 15:43:53 +0530

    Update Contact page

--- webfrontend/src/components/static/Contact.js
+++ webfrontend/src/components/static/Contact.js
@@ -2,10 +2,11 @@ import React from "react";
 
 // CSS file
 import "./Contact.css";
+import img1 from '../../assets/images/Contact/contact-png.png';
 
 function Contact() {
   return (
-    <div>
+    /*<div>
       <h1>This is contact page</h1>
       <div className="content">
         <form action="#">
@@ -34,7 +35,47 @@ function Contact() {
           <input className="button" type="submit" value="send" />
         </form>
       </div>
+    </div>*/
+    <div>
+
+<section className="contact" id="contact">
+
+<div className="image">
+    <img src="images/contact-img.png" alt=""/>
+</div>
+
+<form action="">
+
+    <h1 className="heading">contact us</h1>
+
+    <div className="inputBox">
+        <input type="text" required/>
+        <label>name</label>
     </div>
+
+    <div className="inputBox">
+        <input type="email" required/>
+        <label>email</label>
+    </div>
+
+    <div className="inputBox">
+        <input type="number" required/>
+        <label>phone</label>
+    </div>
+
+    <div className="inputBox">
+        <textarea required name="" id="" cols="30" rows="10"></textarea>
+        <label>message</label>
+    </div>
+
+    <input type="submit" className="btn" value="send message"/>
+
+</form>
+
+</section>
+
+     </div>
+    
   );
 }
 
