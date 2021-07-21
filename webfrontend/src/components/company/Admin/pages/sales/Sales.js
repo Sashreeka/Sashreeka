a59e1da7c68c67b57fccc3a2056c7853dab28896
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-07-21 10:08:36 +0530

    bugs fixed

--- webfrontend/src/components/company/Admin/pages/sales/Sales.js
+++ webfrontend/src/components/company/Admin/pages/sales/Sales.js
@@ -4,12 +4,8 @@ import Sidebar from '../../components/sidebar/Sidebar';
 import {UserData} from '../../dummyData';
 import Chart from '../../components/chart/Chart';
 import SalesDetails from './salesDetails/SalesDetails';
-<<<<<<< Updated upstream
+
 import { BarChart } from '@material-ui/icons';
-=======
-import PieChartNew from '../../components/chart/pieChart/PieChartNew';
-import BarChartNew from '../../components/chart/BarChartNew/BarChartNew';
->>>>>>> Stashed changes
 
 export default function Sales() {
     return (
@@ -17,7 +13,7 @@ export default function Sales() {
             <Sidebar/>
 
              <div className="sales">
-<<<<<<< Updated upstream
+
                    
                 <div className="sales_analytics">
                 <Chart data={UserData} title="User Analytics" grid dataKey="Active User"/>
@@ -25,24 +21,12 @@ export default function Sales() {
                 <div className="sales_barchart">
                 <BarChart/>
                 </div>
-             
-=======
 
-             <div className="top">
-                 <div className="pie">
-                 <PieChartNew/>
-
-                 </div>
-                 <div className="bar">
-                 <BarChartNew/>
-                     
-                 </div>
-             </div>
              
             
 
              <Chart data={UserData} title="User Analytics" grid dataKey="Active User"/>
->>>>>>> Stashed changes
+
              <SalesDetails/>
             
 
