3ec2d7fb5a888f7201e8661b6cfdd0dd890ba5e3
shamalipathirana <shyamalee.123.pathirana@gmail.com>
2021-07-22 12:14:21 +0530

    Update footer.js

--- webfrontend/src/components/common/footer/footer.js
+++ webfrontend/src/components/common/footer/footer.js
@@ -38,8 +38,7 @@ function Footer() {
             farming required an increased focus on providing an all-natural
             solution to boost soil nutrition. Rather than make unproven claims
             about our fertilizer, we focus on education,testing, and proper use
-            to document results. This process may be slower, but it ensures that
-            we support the growth of sustainable agriculture around the world.
+            to document results. 
                 </p>
               </div>
 
