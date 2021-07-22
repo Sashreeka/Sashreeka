cde9e84af4ca5965b941c11060808c6610bbb923
shamalipathirana <shyamalee.123.pathirana@gmail.com>
2021-07-22 10:02:00 +0530

    Changed Contact page

--- webfrontend/src/components/static/Contact.js
+++ webfrontend/src/components/static/Contact.js
@@ -1,52 +1,54 @@
 import React from "react";
-
-// CSS file
 import "./Contact.css";
-import contactImg1 from '../../assets/images/Contact/contact3.png';
+import contactImg1 from '../../assets/images/Contact/contact5.jpg';
 
 function Contact() {
   return (
-    
+    <div>
+    <div className="mainSection">
+        <section className="contact" id="contact">
+            <div className="leftSection">
+                <div className="contact-image">
+                    <img src={contactImg1}/>
+                </div>
 
-<div>
-<section className="contact" id="contact">
+                <div class="mapouter">
+                    <iframe src="https://maps.google.com/maps?q=No%2035%20Reid%20Avenue%20Colombo%207%20&t=&z=15&ie=UTF8&iwloc=&output=embed" >
+                    </iframe>
+                </div>
+            </div>
 
-<div className="image">
-    <img src={contactImg1}/>
-</div>
-<div className="formSection"> 
-<form action="">
+            <div className="formSection"> 
+                <form action="">
+                    <h1 className="contact-heading">Contact Us</h1>
+                    <div className="contact-inputBox">
+                        <input type="text" required/>
+                        <label>Your Name</label>
+                    </div>
 
-    <h1 className="heading">Contact Us</h1>
+                    <div className="contact-inputBox">
+                        <input type="email" required/>
+                        <label>Your Email</label>
+                    </div>
 
-    <div className="inputBox">
-        <input type="text" required/>
-        <label>Your Name</label>
+                    <div className="contact-inputBox">
+                        <input type="number" required/>
+                        <label>Phone</label>
+                    </div>
+
+                    <div className="contact-inputBox">
+                        <textarea required name="" id="" cols="30" rows="10"></textarea>
+                        <label>Message</label>
+                    </div>
+
+                    <input type="submit" className="contactbtn" value="Send"/>
+
+                </form>
+            </div>
+
+        </section>
     </div>
-
-    <div className="inputBox">
-        <input type="email" required/>
-        <label>Your Email</label>
     </div>
-
-    <div className="inputBox">
-        <input type="number" required/>
-        <label>Phone</label>
-    </div>
-
-    <div className="inputBox">
-        <textarea required name="" id="" cols="30" rows="10"></textarea>
-        <label>Message</label>
-    </div>
-
-    <input type="submit" className="contactbtn" value="Send"/>
-
-</form>
-</div>
-</section>
-
-
-</div>
     
   );
 }
