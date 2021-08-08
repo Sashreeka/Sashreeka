8453016418e1c0991b5131b98bbdda3cf2015883
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-08-08 13:42:07 +0530

    updated product page

--- webfrontend/src/components/company/Admin/pages/productList/ProductList.js
+++ webfrontend/src/components/company/Admin/pages/productList/ProductList.js
@@ -68,7 +68,7 @@ export default function ProductList() {
 
 
     const columns=[
-        {title:'Id',field:'userId',
+        {title:'Id',field:'fertilizerId',
     
     
         cellStyle:{
@@ -81,17 +81,33 @@ export default function ProductList() {
     
       
     },
-        {title:'Full Name',field:'name',
+
+    { title: '', field: 'photo',
+     render: item => <img src={item.photo} alt=""  height="30" width="30" style={{borderRadius:20}}
+
+     
+      />,
+      
+      cellStyle:{
+         
+    
+       //  backgroundColor:'green',
+        width:'0%',
+        paddingRight:0,
+     }},
+
+        {title:'Product',field:'name',
     
         cellStyle:{
     
-           // backgroundColor:'red',
-           width:'18%',
+         //backgroundColor:'red',
+           width:'22%',
+           paddingLeft:0
         }
       
     
     },
-        {title:'Is present',field:'active',
+        {title:'Stock',field:'stock',
     
     
         cellStyle:{
@@ -101,7 +117,7 @@ export default function ProductList() {
             
          }
     },
-        {title:'NIC',field:'nic',
+        {title:'Price',field:'unitPrice',
     
     
         cellStyle:{
@@ -109,13 +125,13 @@ export default function ProductList() {
             // backgroundColor:'red',
             width:'20%',
          }},
-        {title:'Phone Number',field:'phoneNumber',
+        {title:'Offer',field:'offer',
     
     
         cellStyle:{
     
             // backgroundColor:'red',
-            width:'25%',
+            width:'15%',
          }},
        
       
@@ -130,13 +146,63 @@ export default function ProductList() {
                 {/* <DataGrid rows={data} disableSelectionOnClick columns={columns} pageSize={8} checkboxSelection /> */}
 
 
-                {
-                    data.map((item)=>(
-                        <div>
-                            <p>{item.name}</p>
-                        </div>
-                    ))
-                }
+              
+
+                <MaterialTable
+
+                    title="Product & Categories Details"
+                    data={data}
+                    columns={columns}
+
+                    options={{
+                    search:true,
+                    paging:true,
+                    filtering:false,
+                    exportButton:true,
+                    backgroundColor: '#EEE',
+                    // showTitle: false,
+
+                    actionsColumnIndex: -1,
+                    headerStyle: {
+                    //width:20,
+                    //marginLeft:20,
+
+                    //  backgroundColor:'red',
+                    color:"#000",
+                    fontWeight:'bold',
+                    width:'15%',
+                    }
+
+
+
+
+
+                    
+                    }}
+
+
+
+                    actions={[
+                        {
+                    icon: 'edit',
+
+                    tooltip: 'Edit',
+                    onClick: (event, rowData) => {
+                        window.location.href='/staffCheck/'+rowData.userId
+                    }
+                    //  <Link to={"/user/" + params.row.id}>
+                    //   <button className="userListEdit">Edit</button>
+                    // </Link>
+
+                    // alert('You are editing ' + rowData.userId)
+                    },
+
+                    ]}
+
+
+
+
+                    />
 
             
         </div>
