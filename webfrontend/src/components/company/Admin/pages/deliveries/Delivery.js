099dbdd247e21f66d3fc91affb4a491353bb2876
Anuki-UCSC <2018cs030@stu.ucsc.cmb.ac.lk>
2021-09-06 11:09:56 +0530

    Deliveries - tabs

--- webfrontend/src/components/company/Admin/pages/deliveries/Delivery.js
+++ webfrontend/src/components/company/Admin/pages/deliveries/Delivery.js
@@ -1,123 +1,147 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./delivery.css";
-import { Link } from "react-router-dom";
 import Sidebar from "../../components/sidebar/Sidebar";
-import axios from 'axios';
 
-import LineChart from "../../components/chart/LineChart";
-import DoughnutChart from "../../components/chart/DoughnutChart";
-import MaterialTable from 'material-table';
+import PropTypes from "prop-types";
+import SwipeableViews from "react-swipeable-views";
+import {
+  Box,
+  Typography,
+  Tab,
+  Tabs,
+  AppBar,
+  makeStyles,
+  useTheme,
+} from "@material-ui/core";
+import { green } from "@material-ui/core/colors";
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+  },
+  tab: {
+    backgroundColor: "#f2f4f2",
+    indicatorColor: "#00ff00",
+    color: "#00ff00",
+  },
+  tabtable: {
+    backgroundColor: "#f3f5f3",
+    fontWeight: "bold",
+    textTransform: "capitalize",
+    "&:hover": {
+      color: "#008000",
+      opacity: 1,
+      backgroundColor: "#d7ded7",
+    },
+    "&:focus": {
+      color: "##008000",
+      borderColor: "#003300",
+      backgroundColor: "#a1b1a1",
+    },
+    "&:select": {
+      color: "#008000",
+      borderColor: "#003300",
+      backgroundColor: "#a1b1a1",
+    },
+  },
+}));
 
 export default function Delivery() {
-  
-  const [data, setData] = useState([]);
+  const classes = useStyles();
+  const theme = useTheme();
+  const [value, setValue] = React.useState(0);
 
-  useEffect(()=>{
-    axios.get("http://localhost:4000/admin/viewDeliveryDetails").then((response)=>{
-      console.log('hi',response.data);
-      setData(response.data);
-    })
-  },[])
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
-  const columns=[{
-      title:'Id',
-      field:'orderId',
-      cellStyle:{
-            width:'10%',
-            } 
-        },
-        {
-        title:'Farmer',
-        field:'famerPhoneNumber',
-        cellStyle:{
-        width:'20%',
-            }
-        }, 
-        {title:'Farmer-Tel',
-        field:'famerPhoneNumber',
-        cellStyle:{
-        width:'13%',
-            }
-        },
-        {title:'delivery Load',
-        field:'deliveryLoad',
-        cellStyle:{
-        width:'10%',
-            }
-        },
-        {title:'distance',
-        field:'distance',
-        cellStyle:{
-        width:'10%',
-        }
-        },
-        {title:'delivering date',
-        field:'dateTime',
-        cellStyle:{
-        width:'10%',
-        }
-        },
-        {title:'status',
-        field:'ConfirmationFlag',
-        cellStyle:{
-         width:'10%',
-        }
-        },
-  ]
+  const handleChangeIndex = (index) => {
+    setValue(index);
+  };
 
-
-
-
-
-    return(
-        <div className="deliveryCon">
-            <Sidebar title="delivery"/>
-            <div>
-
-            {/* *******charts**************** */}
-            <div style={{display:"flex"}}>
-                <div className="lineChart">
-                    <LineChart/>
-                </div>
-                <div className="doughnutChart">
-                    <DoughnutChart/>
-                </div>
-            </div>
-
-            {/* *******table****************** */}
-            <div className="delivery">
-                <MaterialTable
-                title="Company delivery Details"
-                data={data}
-                columns={columns}
-
-                options={{
-                search:true,
-                paging:true,
-                filtering:false,
-                exportButton:true,
-                backgroundColor: '#EEE',
-                actionsColumnIndex: -1,
-                headerStyle: {
-                color:"#000",
-                fontWeight:'bold',
-                width:'15%',
-                }
-                }}
-
-                actions={[{
-                icon: 'edit',
-                tooltip: 'Edit',
-                onClick: (event, rowData) => {
-                    window.location.href='/deliveryCheck/'+rowData.userId
-                }},
-                ]}
-                />
-                    
-                
-                <button>Create New delivery</button>  
-            </div>
-            </div>
+  return (
+    <div className="deliveryCon">
+      <Sidebar title="delivery" />
+      <div style={{ flex: 4 }}>
+        <div className={classes.root}>
+          <AppBar position="static">
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              indicatorColor="primary"
+              textColor="primary"
+              //   variant="fullWidth"
+              aria-label="full width tabs example"
+              className={classes.tab}
+            >
+              <Tab
+                label="Delivery History"
+                {...a11yProps(0)}
+                className={classes.tabtable}
+              />
+              <Tab
+                label="Agents"
+                {...a11yProps(1)}
+                className={classes.tabtable}
+              />
+              <Tab
+                label="Assign Deliveries"
+                {...a11yProps(2)}
+                className={classes.tabtable}
+              />
+            </Tabs>
+          </AppBar>
+          <SwipeableViews
+            axis={theme.direction === "rtl" ? "x-reverse" : "x"}
+            index={value}
+            onChangeIndex={handleChangeIndex}
+          >
+            <TabPanel value={value} index={0} dir={theme.direction}>
+              Item One
+            </TabPanel>
+            <TabPanel value={value} index={1} dir={theme.direction}>
+              Item Two
+            </TabPanel>
+            <TabPanel value={value} index={2} dir={theme.direction}>
+              Item Three
+            </TabPanel>
+          </SwipeableViews>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
