1908b8bb6050b5c94868ae86c55b38cb067532f2
Anuki-UCSC <2018cs030@stu.ucsc.cmb.ac.lk>
2021-09-10 12:42:28 +0530

    MD Bootstrap

--- webfrontend/src/components/common/Control.js
+++ webfrontend/src/components/common/Control.js
@@ -3,12 +3,16 @@
 import Button from "./buttons/Button";
 import Label from "./labels/Label";
 import Input from "./form_Elements/Input";
+import Upload from "./form_Elements/Upload";
+import Select from "./form_Elements/Select";
 // import Checkbox from "./form_Elements/Checkbox";
 
 const Control = {
   Button,
   Label,
   Input,
+  Upload,
+  Select,
   // Checkbox,
 };
 
--- webfrontend/src/components/common/form_Elements/Input.js
+++ webfrontend/src/components/common/form_Elements/Input.js
@@ -1,18 +1,28 @@
 import React from "react";
 import { TextField } from "@material-ui/core";
+import { MDBInput } from "mdbreact";
 
 export default function Input(props) {
   const { name, label, value, error = null, onChange, ...other } = props;
 
   return (
-    <TextField
-      variant="outlined"
+    <MDBInput
+      outline
       label={label}
       name={name}
       value={value}
       onChange={onChange}
       {...other}
-      {...(error && { error: true, helperText: error })}
     />
+
+    // <TextField
+    //   variant="outlined"
+    //   label={label}
+    //   name={name}
+    //   value={value}
+    //   onChange={onChange}
+    //   {...other}
+    //   {...(error && { error: true, helperText: error })}
+    // />
   );
 }
