5f6327e4ee04e64e4c09ede8991126f3e2623d79
Anuki-UCSC <2018cs030@stu.ucsc.cmb.ac.lk>
2021-09-17 07:59:23 +0530

    updated

--- webfrontend/src/components/static/Contact.js
+++ webfrontend/src/components/static/Contact.js
@@ -2,12 +2,9 @@ import React, { useState } from "react";
 import "./Contact.css";
 import contactImg1 from "../../assets/images/Contact/contact5.jpg";
 import axios from "axios";
-<<<<<<< HEAD
-=======
 //import { userSchema } from "./validations/UserValidation";
 //import * as yup from "yup";
 //import {formik From}  from "formik";
->>>>>>> 6a0c141acbc124b4b4bf4c7bc9859ffed4717d0d
 
 import Navigation from "../../components/common/header/Navigation";
 import Footer from "../../components/common/footer/footer";
@@ -38,62 +35,93 @@ function Contact() {
       });
   };
 
-    
+  const onSubmit = () => {
+    axios
+      .post("http://localhost:4000/user/contactus", { values })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-const onSubmit =()=>{
-    axios.post("http://localhost:4000/user/contactus",{values})
-    .then((res)=>{console.log(res)})
-    .catch((err)=>{console.log(err)})
-}
-    
   return (
     <div>
-    
-        <Navigation />
-        <div className="mainSection">
-            <section className="contact" id="contact">
-                <div className="leftSection">
-                    <div className="contact-image">
-                        <img src={contactImg1}/>
-                    </div>
+      <Navigation />
+      <div className="mainSection">
+        <section className="contact" id="contact">
+          <div className="leftSection">
+            <div className="contact-image">
+              <img src={contactImg1} />
+            </div>
 
-                    <div class="mapouter">
-                        <iframe src="https://maps.google.com/maps?q=No%2035%20Reid%20Avenue%20Colombo%207%20&t=&z=15&ie=UTF8&iwloc=&output=embed" >
-                        </iframe>
-                    </div>
-                </div>
+            <div class="mapouter">
+              <iframe src="https://maps.google.com/maps?q=No%2035%20Reid%20Avenue%20Colombo%207%20&t=&z=15&ie=UTF8&iwloc=&output=embed"></iframe>
+            </div>
+          </div>
 
-                <div className="formSection"> 
-                    <form action="">
-                        <h1 className="contact-heading">Contact Us</h1>
-                        <div className="contact-inputBox">
-                            <input type="text" name="name" value={values.name} onChange={handleInputChange} required/>
-                            <label>Your Name</label>
-                        </div>
+          <div className="formSection">
+            <form action="">
+              <h1 className="contact-heading">Contact Us</h1>
+              <div className="contact-inputBox">
+                <input
+                  type="text"
+                  name="name"
+                  value={values.name}
+                  onChange={handleInputChange}
+                  required
+                />
+                <label>Your Name</label>
+              </div>
 
-                        <div className="contact-inputBox">
-                            <input type="email" name="email" value={values.email} onChange={handleInputChange} required/>
-                            <label>Your Email</label>
-                        </div>
+              <div className="contact-inputBox">
+                <input
+                  type="email"
+                  name="email"
+                  value={values.email}
+                  onChange={handleInputChange}
+                  required
+                />
+                <label>Your Email</label>
+              </div>
 
-                        <div className="contact-inputBox">
-                            <input type="number" name="phone" value={values.phone} onChange={handleInputChange} required/>
-                            <label>Phone</label>
-                        </div>
+              <div className="contact-inputBox">
+                <input
+                  type="number"
+                  name="phone"
+                  value={values.phone}
+                  onChange={handleInputChange}
+                  required
+                />
+                <label>Phone</label>
+              </div>
 
-                        <div className="contact-inputBox">
-                            <textarea required name="" id="" cols="30" rows="10" name="massage" value={values.massage} onChange={handleInputChange}></textarea>
-                            <label>Message</label>
-                        </div>
+              <div className="contact-inputBox">
+                <textarea
+                  required
+                  name=""
+                  id=""
+                  cols="30"
+                  rows="10"
+                  name="massage"
+                  value={values.massage}
+                  onChange={handleInputChange}
+                ></textarea>
+                <label>Message</label>
+              </div>
 
-                        <input type="submit" className="contactbtn" value="Send" onclick={onSubmit}/>
-
-                    </form>
-                </div>
-
-            </section>
-        </div>
-        <Footer />
+              <input
+                type="submit"
+                className="contactbtn"
+                value="Send"
+                onclick={onSubmit}
+              />
+            </form>
+          </div>
+        </section>
+      </div>
+      <Footer />
     </div>
   );
 }
