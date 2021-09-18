ca3462d1a25cdb03c678feab83545ebe4f3b8cc2
Anuki-UCSC <2018cs030@stu.ucsc.cmb.ac.lk>
2021-09-18 08:22:37 +0530

    contactUs backend success

--- Backend/routes/ContactUs.js
+++ Backend/routes/ContactUs.js
@@ -2,38 +2,25 @@ const express = require("express");
 const router = express.Router();
 const db = require("../connection/database");
 
+router.post("/user/contactus", (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const phoneNumber = req.body.phone;
+  const massage = req.body.massage;
 
-router.post("/user/contactus",(req,res)=>{
-    const name = req.body.name;
-    const email = req.body.email;
-    const phoneNumber =req.body.phone;
-    const massage=req.body.massage;
+  const sqlInsert =
+    "INSERT INTO contactus(name, email, phoneNumber, massage) VALUES (?,?,?,?)";
 
-
-    const sqlInsert="INSERT INTO contactus(name, email, phoneNumber, massage) VALUES (?,?,?,?)";
-
-    db.query(
-        sqlInsert,
-        [
-            name,email,phoneNumber,massage
-        ],
-        (err,result)=>{
-            if(err){
-                //res.send({ err: err });
-            console.log(err);
-            }
-            
-        }
-    );
+  db.query(sqlInsert, [name, email, phoneNumber, massage], (err, result) => {
+    if (err) {
+      //res.send({ err: err });
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
 });
 
-
-
-
-
-
 //INSERT INTO `contactus`(`id`, `name`, `email`, `phoneNumber`, `massage`) VALUES ([value-1],[value-2],[value-3],[value-4],[value-5])
-
-
 
 module.exports = router;
