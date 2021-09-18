32eee1fbd1209c6b248125c76fabb040b393867b
ChamodhiSatharasinghe <56638407+ChamodhiSatharasinghe@users.noreply.github.com>
2021-09-18 17:38:56 +0530

    fertilizer store home backend

--- Backend/routes/store/Store.js
+++ Backend/routes/store/Store.js
@@ -2,9 +2,27 @@ const express = require("express");
 const router = express.Router();
 const db = require("../../connection/database");
 
-router.get("/getstore", (req, res) => {
+router.get("/getnewarrival", (req, res) => {
 
-    const sqlgetfer = "SELECT * FROM fertilizer";
+    const sqlgetfer = "SELECT * FROM fertilizer ORDER BY arrivalTime DESC LIMIT 7";
+    db.query(sqlgetfer, (err, result) => {
+       // console.log(result);
+       res.send(result);
+    });
+  });
+
+  router.get("/getpopular", (req, res) => {
+
+    const sqlgetfer = "SELECT * FROM fertilizer a INNER JOIN (SELECT fertilizerId FROM ordercontainsfertilizer GROUP BY fertilizerId ORDER BY COUNT(fertilizerId) DESC LIMIT 10) b ON a.fertilizerId=b.fertilizerId";
+    db.query(sqlgetfer, (err, result) => {
+       // console.log(result);
+       res.send(result);
+    });
+  });
+
+  router.get("/getoffer", (req, res) => {
+
+    const sqlgetfer = "SELECT * FROM fertilizer WHERE offer>0";
     db.query(sqlgetfer, (err, result) => {
        // console.log(result);
        res.send(result);
