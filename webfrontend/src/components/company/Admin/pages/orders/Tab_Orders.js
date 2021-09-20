50a13ada5107d0578dc5c6e50454ea7bb3f8c77f
Anuki-UCSC <2018cs030@stu.ucsc.cmb.ac.lk>
2021-09-20 12:36:12 +0530

    updated

--- webfrontend/src/components/company/Admin/pages/orders/Tab_Orders.js
+++ webfrontend/src/components/company/Admin/pages/orders/Tab_Orders.js
@@ -2,41 +2,87 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { DataGrid } from "@material-ui/data-grid";
 
+// const columns = [
+//   { field: "id", headerName: "ID", width: 90 },
+//   {
+//     field: "address",
+//     headerName: "Address",
+//     width: 170,
+//     // editable: true,
+//   },
+//   {
+//     field: "district",
+//     headerName: "District",
+//     width: 150,
+//     // editable: true,
+//   },
+//   {
+//     field: "quickFlag",
+//     headerName: "Quick",
+//     width: 80,
+//     // render: (row) => (
+//     //   <div>{row.quickFlag == 1 ? <Chip size="small" label="Quick" /> : ""}</div>
+//     // ),
+//     // editable: true,
+//   },
+//   {
+//     field: "loads",
+//     headerName: "Load",
+//     width: 80,
+//     // editable: true,
+//   },
+//   {
+//     field: "graceenddate",
+//     headerName: "Delivery Deadline",
+//     width: 110,
+//     // editable: true,
+//   },
+// ];
+
 const columns = [
   { field: "id", headerName: "ID", width: 90 },
   {
-    field: "address",
-    headerName: "Address",
-    width: 170,
-    // editable: true,
-  },
-  {
-    field: "district",
-    headerName: "District",
+    field: "firstName",
+    headerName: "First name",
     width: 150,
-    // editable: true,
+    editable: true,
   },
   {
-    field: "quickFlag",
-    headerName: "Quick",
-    width: 80,
-    // render: (row) => (
-    //   <div>{row.quickFlag == 1 ? <Chip size="small" label="Quick" /> : ""}</div>
-    // ),
-    // editable: true,
+    field: "lastName",
+    headerName: "Last name",
+    width: 150,
+    editable: true,
   },
   {
-    field: "loads",
-    headerName: "Load",
-    width: 80,
-    // editable: true,
-  },
-  {
-    field: "graceenddate",
-    headerName: "Delivery Deadline",
+    field: "age",
+    headerName: "Age",
+    type: "number",
     width: 110,
-    // editable: true,
+    editable: true,
   },
+  {
+    field: "fullName",
+    headerName: "Full name",
+    description: "This column has a value getter and is not sortable.",
+    sortable: false,
+    width: 160,
+    valueGetter: (params) =>
+      `${params.getValue(params.id, "firstName") || ""} ${
+        params.getValue(params.id, "lastName") || ""
+      }`,
+  },
+];
+
+const rows = [
+  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
+  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
+  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
+  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
+  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
 ];
 
 export default function TabOrders() {
@@ -55,14 +101,16 @@ export default function TabOrders() {
 
   return (
     <div>
-      <DataGrid
-        rows={orders}
-        columns={columns}
-        pageSize={5}
-        rowsPerPageOptions={[5]}
-        checkboxSelection
-        disableSelectionOnClick
-      />
+      <div style={{ height: 400, width: "100%" }}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          pageSize={5}
+          rowsPerPageOptions={[5]}
+          checkboxSelection
+          disableSelectionOnClick
+        />
+      </div>
     </div>
   );
 }
